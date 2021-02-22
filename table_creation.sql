1a51b9c7532d8b771ca5d4f488b6df9d55700d84
NaderSalameh <74309258+NaderSalameh@users.noreply.github.com>
2021-02-22 14:21:59 -0500

    Update table_creation.sql

--- table_creation.sql
+++ table_creation.sql
@@ -5,9 +5,9 @@
 --. creation of title table 
 DROP TABLE IF EXISTS Title;
 	CREATE TABLE Title 
-	(--.-Name----------------.Type-------------.NOT NULL/NULL-----------
-		  title_id			  CHAR(5)           NOT NULL     PRIMARY KEY 
-	   	, title               VARCHAR(25)       NOT NULL  	
+	(--.--Name----------------.Type-------------.NOT NULL/NULL-----------
+	     title_id		   CHAR(5)           NOT NULL     PRIMARY KEY 
+	   , title                 VARCHAR(25)       NOT NULL  	
 	);
 
 
@@ -15,8 +15,8 @@ DROP TABLE IF EXISTS Title;
 DROP TABLE IF EXISTS Department;
 	CREATE TABLE Department 
 	(--.--Name----------------.Type-------------.NOT NULL/NULL-----------
-		  dept_no			   CHAR(4)           NOT NULL     PRIMARY KEY 
-	   	, dept_name            VARCHAR(25)       NOT NULL  	
+	      dept_no	           CHAR(4)           NOT NULL     PRIMARY KEY 
+	   ,  dept_name            VARCHAR(25)       NOT NULL  	
 	);
 
 
@@ -24,13 +24,13 @@ DROP TABLE IF EXISTS Department;
 DROP TABLE IF EXISTS Employee;
 	CREATE TABLE Employee 
 	(--.--Name----------------.Type-------------.NOT NULL/NULL-----------
-		  emp_no			   INTEGER           NOT NULL  	  PRIMARY KEY 
-	   	, emp_title_id         CHAR(5)           NOT NULL,    FOREIGN KEY (emp_title_id) REFERENCES	Title (title_id)
-	   	, birth_date           DATE              NOT NULL 
-		, first_name           VARCHAR(25)       NOT NULL 
-		, last_name            VARCHAR(75)       NOT NULL 
-		, sex                  CHAR(1)           NOT NULL 
-		, hire_date            DATE              NOT NULL 
+	     emp_no	           INTEGER           NOT NULL  	  PRIMARY KEY 
+	   , emp_title_id          CHAR(5)           NOT NULL,    FOREIGN KEY (emp_title_id) REFERENCES	Title (title_id)
+	   , birth_date            DATE              NOT NULL 
+	   , first_name            VARCHAR(25)       NOT NULL 
+	   , last_name             VARCHAR(75)       NOT NULL 
+	   , sex                   CHAR(1)           NOT NULL 
+	   , hire_date             DATE              NOT NULL 
 	);
 
 
@@ -38,10 +38,10 @@ DROP TABLE IF EXISTS Employee;
 DROP TABLE IF EXISTS Salary;
 	CREATE TABLE Salary 
 	(--.--Name----------------.Type-------------.NOT NULL/NULL-----------
-		  emp_no			   INTEGER           NOT NULL,    FOREIGN KEY (emp_no) REFERENCES Employee (emp_no)
-	   	, salary               VARCHAR(25)       NOT NULL  
+	      emp_no	           INTEGER           NOT NULL,    FOREIGN KEY (emp_no) REFERENCES Employee (emp_no)
+	   ,  salary               VARCHAR(25)       NOT NULL  
 		
-		, PRIMARY KEY(emp_no)	
+	   , PRIMARY KEY(emp_no)	
 	);
 
 
@@ -49,10 +49,10 @@ DROP TABLE IF EXISTS Salary;
 DROP TABLE IF EXISTS Dept_Emp;
 	CREATE TABLE Dept_Emp 
 	(--.--Name----------------.Type-------------.NOT NULL/NULL-----------
-		  emp_no               INTEGER           NOT NULL,    FOREIGN KEY (emp_no)  REFERENCES Employee (emp_no) 
-		, dept_no		       CHAR(4)           NOT NULL,    FOREIGN KEY (dept_no) REFERENCES Department (dept_no)
+	      emp_no               INTEGER           NOT NULL,    FOREIGN KEY (emp_no)  REFERENCES Employee (emp_no) 
+	    , dept_no		   CHAR(4)           NOT NULL,    FOREIGN KEY (dept_no) REFERENCES Department (dept_no)
 		
-		, PRIMARY KEY(emp_no, dept_no) 
+	    , PRIMARY KEY(emp_no, dept_no) 
 	);
 
 
@@ -60,10 +60,10 @@ DROP TABLE IF EXISTS Dept_Emp;
 DROP TABLE IF EXISTS Dept_Manager;
 	CREATE TABLE Dept_Manager 
 	(--.--Name----------------.Type-------------.NOT NULL/NULL-----------
-		  dept_no              CHAR(4)           NOT NULL,    FOREIGN KEY (dept_no) REFERENCES Department (dept_no)    
-	   	, emp_no               INTEGER           NOT NULL,    FOREIGN KEY (emp_no)  REFERENCES Employee (emp_no)
+	      dept_no              CHAR(4)           NOT NULL,    FOREIGN KEY (dept_no) REFERENCES Department (dept_no)    
+	    , emp_no               INTEGER           NOT NULL,    FOREIGN KEY (emp_no)  REFERENCES Employee (emp_no)
 		
-		, PRIMARY KEY(dept_no, emp_no) 
+	    , PRIMARY KEY(dept_no, emp_no) 
 	);
  
 
